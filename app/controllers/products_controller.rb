a56bc4130d878c702ec851ea17aa162041c42016
Erika Gonzalez <erikagonzz01@gmail.com>
2023-12-12 15:09:24 -0500

    products show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,9 @@ class ProductsController < ApplicationController
     @products = Product.all
     render template: "products/index"
   end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    render template: "products/show"
+  end
 end

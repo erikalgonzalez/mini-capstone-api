59d351ff73d4988c6684b6610dab266d61b28bff
Erika Gonzalez <erikagonzz01@gmail.com>
2023-12-12 14:58:35 -0500

    products index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,19 +3,4 @@ class ProductsController < ApplicationController
     @products = Product.all
     render template: "products/index"
   end
-
-  def show
-    @product = Product.first
-    render template: "products/show"
-  end
-
-  def show
-    @product = Product.second
-    render template: "products/show"
-  end
-
-  def show
-    @product = Product.third
-    render template: "products/show"
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/products", controller: "products", action: "index"
-  get "/first_product", controller: "products", action: "show"
-  get "/second_product", controller: "products", action: "show"
-  get "/third_product", controller: "products", action: "show"
+  get "/products" => "products#index"
 end

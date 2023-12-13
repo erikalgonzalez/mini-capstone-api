b87e31b703eedc2b58d3dbdd896779ecbba80abf
Erika Gonzalez <erikagonzz01@gmail.com>
2023-12-12 22:11:04 -0500

    changed data type for price and description, added inventory attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_12_180812) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_13_023201) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
-    t.string "description"
+    t.decimal "price", precision: 7, scale: 2
+    t.text "description"
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "inventory"
   end
 
 end
